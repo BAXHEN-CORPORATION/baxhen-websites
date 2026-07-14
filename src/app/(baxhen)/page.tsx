0f04c6efa8c4d2c1cbf826596fe0c8c469b5dfc9
Leonardo Rebouças Batista <reboucas.beraby@gmail.com>
2026-07-14 01:26:00 +0100

    last try

--- src/app/(baxhen)/page.tsx
+++ src/app/(baxhen)/page.tsx
@@ -2,6 +2,8 @@ import { redirect } from 'next/navigation'
 import { headers } from 'next/headers'
 import { SUPPORTED_LOCALES } from '@/domain/shared/types'
 
+export const dynamic = 'force-dynamic'
+
 export default async function BaxhenRoot() {
   const acceptLanguage = (await headers()).get('accept-language') || ''
   const preferred = acceptLanguage.split(',')[0]?.split('-')[0]?.toLowerCase()
--- src/app/(baxhen)/websites/page.tsx
+++ src/app/(baxhen)/websites/page.tsx
@@ -2,6 +2,8 @@ import { redirect } from 'next/navigation'
 import { headers } from 'next/headers'
 import { SUPPORTED_LOCALES } from '@/domain/shared/types'
 
+export const dynamic = 'force-dynamic'
+
 /**
  * Redirect /websites to the user's preferred locale.
  * Detects language from Accept-Language header.
