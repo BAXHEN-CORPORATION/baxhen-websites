d9c1d3f00d693bf01e27d00135f4cf172b1836c4
Leonardo Rebouças Batista <reboucas.beraby@gmail.com>
2026-07-14 13:01:35 +0100

    try 2

--- src/app/(baxhen)/layout.tsx
+++ src/app/(baxhen)/layout.tsx
@@ -1,39 +1,20 @@
-import type { Metadata } from 'next'
-import { cn } from '@/utilities/ui'
-import { GeistMono } from 'geist/font/mono'
-import { GeistSans } from 'geist/font/sans'
 import React from 'react'
-
-export const dynamic = 'force-dynamic'
 import { AdminBar } from '@/components/AdminBar'
 import { Providers } from '@/providers'
-import { mergeOpenGraph } from '@/utilities/mergeOpenGraph'
 import { draftMode } from 'next/headers'
-import '@/app/globals.css'
-import { getServerSideURL } from '@/utilities/getURL'
+
+export const dynamic = 'force-dynamic'
 
 export default async function BaxhenLayout({ children }: { children: React.ReactNode }) {
   const { isEnabled } = await draftMode()
   return (
-    <html className={cn(GeistSans.variable, GeistMono.variable)} lang="en" suppressHydrationWarning>
-      <head>
-        {/* eslint-disable-next-line @next/next/no-before-interactive-script-outside-document */}
-        <script dangerouslySetInnerHTML={{ __html: `(function(){var d=document.documentElement;var t='light';var p=localStorage.getItem('payload-theme');if(p==='light'||p==='dark')t=p;else{var m=window.matchMedia('(prefers-color-scheme:dark)');if(m.matches)t='dark'}d.setAttribute('data-theme',t)})()` }} />
-        <link href="/favicon.ico" rel="icon" sizes="32x32" />
-        <link href="/favicon.svg" rel="icon" type="image/svg+xml" />
-      </head>
-      <body>
-        <Providers>
-          <AdminBar adminBarProps={{ preview: isEnabled }} />
-          {children}
-        </Providers>
-      </body>
-    </html>
+    <>
+      {/* eslint-disable-next-line @next/next/no-before-interactive-script-outside-document */}
+      <script dangerouslySetInnerHTML={{ __html: `(function(){var d=document.documentElement;var t='light';var p=localStorage.getItem('payload-theme');if(p==='light'||p==='dark')t=p;else{var m=window.matchMedia('(prefers-color-scheme:dark)');if(m.matches)t='dark'}d.setAttribute('data-theme',t)})()` }} />
+      <Providers>
+        <AdminBar adminBarProps={{ preview: isEnabled }} />
+        {children}
+      </Providers>
+    </>
   )
-}
-
-export const metadata: Metadata = {
-  metadataBase: new URL(getServerSideURL()),
-  openGraph: mergeOpenGraph(),
-  twitter: { card: 'summary_large_image', creator: '@payloadcms' },
 }
